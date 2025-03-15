cd92cba4a09221a273088638806583ade1947bfa
10517822+degobbis@users.noreply.github.com <10517822+degobbis@users.noreply.github.com>
2025-03-15 11:02:27 +0100

    Add script to update hyprland plugins after an hyprland-git update

---
+++ dotfiles/.config/hypr/scripts/gdg-update-hyprland-plugins.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Update hyprland plugins after hyprland-git update
+
+VERSION_FILE="$HOME/.config/hypr/version"
+CURRENT_VERSION=$(hyprctl version | grep "Tag" | awk '{print $2}')
+
+if [[ ! -f "$VERSION_FILE" ]] || [[ "$(cat "$VERSION_FILE")" != "$CURRENT_VERSION" ]]; then
+    notify-send -u critical "Hyprland-Plugins werden aktualisiert..." "Bitte warten, bis das Update abgeschlossen ist."
+
+    # Plugins updaten
+    hyprpm update
+
+    # Falls Update erfolgreich, speichern wir die neue Version
+    if [[ $? -eq 0 ]]; then
+        echo "$CURRENT_VERSION" > "$VERSION_FILE"
+        swaync-client --hide-latest
+        notify-send -u normal "Hyprland-Plugin-Update abgeschlossen!" "Du kannst Hyprland jetzt normal nutzen."
+    else
+        notify-send -u critical "Hyprland-Plugin-Update FEHLGESCHLAGEN!" "Überprüfe mit 'hyprpm update -v' in der Konsole die Fehler."
+    fi
+fi

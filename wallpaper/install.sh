702eae66f8c9fb9f04e52ee1b8761020ce59931d
10517822+degobbis@users.noreply.github.com <10517822+degobbis@users.noreply.github.com>
2025-05-23 16:13:40 +0200

    Updated ML4W to 2.9.8.6

--- wallpaper/install.sh
+++ wallpaper/install.sh
@@ -18,13 +18,34 @@ if [ ! -d .git ]; then
     echo "https://gitlab.com/stephan-raabe/wallpaper/ ?"
     echo ""
     if gum confirm "Do you want to download the repository?" ;then
-        wget -P ~/Downloads/ https://gitlab.com/stephan-raabe/wallpaper/-/archive/main/wallpaper-main.zip
-        unzip -o ~/Downloads/wallpaper-main.zip -d ~/Downloads/
+        if [ -d ~/Downloads/wallpaper-main ]; then
+            rm -Rf ~/Downloads/wallpaper-main
+        fi
+        if [ -f ~/Downloads/main.zip ]; then
+            rm -f ~/Downloads/main.zip
+        fi
+        wget -P ~/Downloads/ https://github.com/mylinuxforwork/wallpaper/archive/refs/heads/main.zip
+        unzip -o ~/Downloads/main.zip -d ~/Downloads/
         if [ ! -d ~/wallpaper/ ]; then
             mkdir ~/wallpaper
         fi
-        cp ~/Downloads/wallpaper-main/* ~/wallpaper/
-        echo "Wallpapers from the repository installed successfully."
+        echo "--> Remove duplicates blackwhole.png, Solitary-Glow.png, the-sphere2.png, 3d-tech.jpg, default.jpg"
+        rm -f ~/Downloads/wallpaper-main/blackwhole.png
+        rm -f ~/Downloads/wallpaper-main/Solitary-Glow.png
+        rm -f ~/Downloads/wallpaper-main/the-sphere2.png
+        rm -f ~/Downloads/wallpaper-main/3d-tech.jpg
+        rm -f ~/Downloads/wallpaper-main/default.jpg
+        chmod 644 ~/Downloads/wallpaper-main/*.*
+        echo "--> Convert *.png to .jpg"
+        mogrify -format jpg ~/Downloads/wallpaper-main/*.png
+        echo "--> Copy JPGs to ~/wallpaper"
+        cp ~/Downloads/wallpaper-main/*.jpg ~/wallpaper/
+        echo "--> Copy LICENSE to ~/wallpaper"
+        cp ~/Downloads/wallpaper-main/LICENSE ~/wallpaper/
+        echo "--> Clean up the downloaded/extracted files in ~/Downloads"
+        rm -f ~/Downloads/main.zip
+        rm -Rf ~/Downloads/wallpaper-main
+        echo ":: Wallpapers from the repository installed successfully."
     elif [ $? -eq 130 ]; then
         exit 130
     else
